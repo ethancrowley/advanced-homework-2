a2bd1963bde77819f677c2ea8eba96f48d611987
ethancrowley <ethan.j.crowley@gmail.com>
2017-08-04 23:55:28 -0500

    homework

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import ImageSlider from "./components/Imageslider.js";
 import ProductDetail from "./components/ProductDetail.js";
+import SubHeader from "./components/SubHeader.js";
+import TopNav from "./components/TopNav.js";
 
 function App(props) {
     return (
@@ -14,32 +16,9 @@ function App(props) {
 
 		<Header />
 			<div className="clear"> </div>
-			<div className="sub-header">
-				<div className="logo">
-					<a href="index.html"><img src="images/logo.png" title="logo" /></a>
-				</div>
-				<div className="sub-header-right">
-					<ul>
-						<li><a href="#">log in</a></li>
-						<li><a href="#">Your account</a></li>
-						<li><a href="#">CART: (EMPTY) <img src="images/cart.png" title="cart" /></a></li>
-					</ul>
-					<input type="text" /><input type="submit"  value="search" />
-				</div>
-				<div className="clear"> </div>
-			</div>
+			<SubHeader />
 			<div className="clear"> </div>
-			<div className="top-nav">
-				<ul>
-					<li><a href="carlights.html">car lights</a></li>
-					<li><a href="carwheels.html">Car wheels</a></li>
-					<li><a href="carbumpers.html">car bumpers</a></li>
-					<li><a href="caradsystem.html">car audiosystem</a></li>
-					<li><a href="truckbumpers.html">Truck bumpers</a></li>
-					<li><a href="contact.html">Feedback</a></li>
-					<div className="clear"> </div>
-				</ul>
-			</div>
+			<TopNav changeCategory={props.changeCategory} />
 
 			</div>
 
